1752b8cd0daa314d363e972927b2960e6466d960
Domi <dominik.seifert@gmail.com>
2015-06-27 22:58:31 +0800

    - added simple sun light rotation and Unit.cs

---
+++ Assets/Sun.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sun : MonoBehaviour {
+	[HideInInspector]
+	public float VirtualTimeOfDay;
+	public float RealSecondsPerDay = 10;
+
+	// Use this for initialization
+	void Start () {
+		// get current time of day
+		var systemTimeOfDay = System.DateTime.Now.TimeOfDay;
+		VirtualTimeOfDay = (float)systemTimeOfDay.TotalDays;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// advance time of day
+		var timeDiff = Time.deltaTime;
+		var virtualTimeDiff = timeDiff / RealSecondsPerDay;
+		VirtualTimeOfDay += virtualTimeDiff;
+		VirtualTimeOfDay = Mathf.Repeat(VirtualTimeOfDay, 1);
+
+		// update rotation
+		//transform.localRotation = 
+		transform.localRotation = Quaternion.Euler(new Vector3(360 * VirtualTimeOfDay,-240,170));
+	}
+}
---
+++ Assets/Unit.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Unit : MonoBehaviour {
+	public Color HighlighColor = Color.yellow;
+
+	private NavMeshAgent agent;
+	private new Renderer renderer;
+
+	private bool isSelected;
+	private Color originalColor;
+
+	Unit() {
+	}
+
+	public NavMeshAgent Agent {
+		get { return agent; }
+		set { agent = value; }
+	}
+	
+	protected void Reset() {
+		Debug.Log("Reset: " + name);
+	}
+
+	protected void Start() {
+		agent = GetComponent<NavMeshAgent>();
+		renderer = GetComponent<Renderer> ();
+	}
+
+	protected void Update() {
+		//IsSelected = !IsSelected;
+		if (Input.GetButton("Fire1")) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray, out hit)) {
+				agent.SetDestination(hit.point);
+			}
+		}
+	}
+
+	public bool IsSelected {
+		get {
+			return isSelected;
+		}
+		set {
+			if (value == isSelected) return;
+
+			if (value) {
+				// select
+				originalColor = renderer.material.color;
+				renderer.material.color = HighlighColor;
+			}
+			else {
+				// de-select
+				renderer.material.color = originalColor;
+			}
+			
+			isSelected = value;
+		}
+	}
+}
